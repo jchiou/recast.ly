07d6c82860d71296b49178b51820b8457425f9a9
jchiou <jessica.chiou@gmail.com>
2016-06-24 19:20:08 -0700

    Passed all VideoPlayer tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,9 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+    {console.log(window.fakeVideoData)}
+      <VideoPlayer video={window.fakeVideoData[0]}/>
+    }
     </div>
     <div className="col-md-5">
       <VideoList videos={window.exampleVideoData} />
